f286b05bb1b3bb4d52c475bd3c19d13f515fda7c
AbhayRjvanshi <143062825+AbhayRjvanshi@users.noreply.github.com>
2024-11-16 15:17:05 +0530

    Update sign.js

--- sign.js
+++ sign.js
@@ -1,21 +1,14 @@
-// Save user data to localStorage
 function signup() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    if (username && password) {
-        // Store user data in localStorage
-        const users = JSON.parse(localStorage.getItem("users")) || {};
-        if (users[username]) {
-            alert("Username already exists! Please choose another.");
-        } else {
-            users[username] = password;
-            localStorage.setItem("users", JSON.stringify(users));
-            alert("Sign-up successful! Redirecting to login...");
-            window.location.href = "login.html";
-        }
+    const users = JSON.parse(localStorage.getItem("users")) || {};
+    if (users[username]) {
+        alert("Username already exists! Please choose another.");
     } else {
-        alert("Please provide both username and password.");
+        users[username] = password;
+        localStorage.setItem("users", JSON.stringify(users));
+        alert("Sign-up successful! Redirecting to login...");
+        window.location.href = "login.html";
     }
 }
-
